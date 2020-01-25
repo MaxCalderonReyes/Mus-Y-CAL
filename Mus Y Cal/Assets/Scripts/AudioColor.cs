d6a021ea31630d751b2c0dde303e1d0304558e5c
Cytanic <maxvalentino@hotmail.com>
2020-01-25 11:15:07 -0500

    Proyecto

---
+++ Mus Y Cal/Assets/Scripts/AudioColor.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioColor : AudioSyncer
+{
+  
+	public Color[] beatColors;
+	public Color restColor;
+
+	private int m_randomIndx;
+	private Color m_img;
+
+
+    private void Start()
+	{
+		m_img = GetComponent<Renderer>().material.color;
+       
+	}
+
+
+	public override void OnBeat()
+	{
+        print(m_img);
+		base.OnBeat();
+
+		Color _c = RandomColor();
+
+		StopCoroutine("MoveToColor");
+		StartCoroutine("MoveToColor", _c);
+	}
+
+    public override void OnUpdate()
+	{
+      
+		base.OnUpdate();
+
+		if (m_isBeat) return;
+
+		m_img = Color.Lerp(m_img, restColor, restSmoothTime*Time.deltaTime);
+	}
+
+    	private Color RandomColor()
+	{
+		if (beatColors == null || beatColors.Length == 0) return Color.white;
+		m_randomIndx = Random.Range(0, beatColors.Length);
+		return beatColors[m_randomIndx];
+	}
+
+    
+	private IEnumerator MoveToColor(Color _target)
+	{
+		Color _curr = m_img;
+		Color _initial = _curr;
+		float _timer = 0;
+		
+		while (_curr != _target)
+		{
+			_curr = Color.Lerp(_initial, _target, _timer / timeToBeat);
+			_timer += Time.deltaTime;
+
+			m_img = _curr;
+
+			yield return null;
+		}
+
+		m_isBeat = false;
+	}
+
+
+
+	
+
+
+
+	
+}
---
+++ Mus Y Cal/Assets/Scripts/AudioSpectrum.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSpectrum : MonoBehaviour
+{
+    private float[] _audioSpectrum;
+    public static float ValueSpectrum {get;private set;}
+  
+    void Start()
+    {
+        _audioSpectrum=new float[128];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+        AudioListener.GetSpectrumData(_audioSpectrum,0,FFTWindow.Rectangular);
+        if(_audioSpectrum!=null && _audioSpectrum.Length>0){
+            ValueSpectrum=_audioSpectrum[0] *100;
+        }
+    }
+}
