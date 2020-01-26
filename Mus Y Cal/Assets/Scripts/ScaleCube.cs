2792da0dfc2cd3f33a5fa2cedbd8f82ae570e9f1
Cytanic <maxvalentino@hotmail.com>
2020-01-26 18:24:33 -0500

    Arreglo 2

--- Mus Y Cal/Assets/Scripts/ScaleCube.cs
+++ Mus Y Cal/Assets/Scripts/ScaleCube.cs
@@ -4,16 +4,40 @@ using UnityEngine;
 
 public class ScaleCube : MonoBehaviour
 {
+ 
+   public Color[] colors;
+   private Color _CubColor;
   public int band;
   public float startScale,scaleMultiplayer;
+  private Renderer TempColor;
+
+  
     void Start()
-    {
-        
+    {	  
+      TempColor = GetComponent<Renderer>();
+        _CubColor=GetComponent<Renderer>().material.color;
     }
 
     // Update is called once per frame
     void Update()
     {
+      TempColor.material.color=_CubColor;
         transform.localScale=new Vector3(transform.localScale.x,(MusicScript.spectrum[band]*scaleMultiplayer)+startScale,transform.localScale.z);
+
+       if(transform.localScale.y>1.0f){
+         _CubColor=colors[0];
+          if(transform.localScale.y>2.0f){
+         _CubColor=colors[1];
+    
+           if(transform.localScale.y>3.0f){
+         _CubColor=colors[2];
+       }
+       }
+       }
+       
+       else{
+         _CubColor=colors[3];
+       }
+
     }
 }
